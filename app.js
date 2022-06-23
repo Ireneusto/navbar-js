f935b8d990f11707b51003089c47d67898f78216
Ireneusto <ireneusto@gmail.com>
2022-06-23 11:24:46 +0200

    link animation added

---
+++ app.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger")
+    const nav = document.querySelector(".nav-links")
+    const navLinks = document.querySelectorAll(".nav-links li")
+
+    hamburger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active")
+
+        navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ""
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.5}s`
+                }
+        })
+        hamburger.classList.toggle("toggle")
+
+    });
+
+}
+
+navSlide()
